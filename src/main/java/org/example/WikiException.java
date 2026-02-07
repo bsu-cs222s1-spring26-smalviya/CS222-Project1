b752c9d7ae494e661b897b327c8e24956ffab618
renfeizhang <daniel.daniel@bsu.edu>
2026-02-07 15:47:29 -0500

    overloaded constructor of wikiexception

--- src/main/java/org/example/WikiException.java
+++ src/main/java/org/example/WikiException.java
@@ -4,4 +4,7 @@ public class WikiException extends Exception {
     public WikiException(String message) {
         super(message);
     }
+    public WikiException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

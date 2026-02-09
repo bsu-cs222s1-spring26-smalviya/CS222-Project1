a719bb7d2c9aeab90325c03454679604a7559285
AlecFeiYe <fei.ye@bsu.edu>
2026-02-09 09:40:48 -0500

    Integrate Input, Requester, and Output classes in Main. Delete EmptyFile.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Input input = new Input();
+        Output output = new Output();
+        Requester requester = new Requester();
+        //RevisionParser parser = new RevisionParser();
+
+        try {
+            String title = input.getUserInput();
+
+            String json = requester.fetchRecentRevisions(title);
+
+            output.print("Raw JSON Data: " + json);
+        } catch (WikiException e) {
+            output.print("Error: " + e.getMessage());
+        }
 
     }
 }

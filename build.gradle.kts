64db64c2f50996581fb09ba0c75abd15f8970d57
renfeizhang <daniel.daniel@bsu.edu>
2026-02-17 11:25:18 -0500

    javafx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
-}
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0" }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -15,9 +16,25 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
     implementation("org.slf4j:slf4j-simple:2.0.9")
-    implementation("net.minidev:json-smart:2.5.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17)) // <- match your installed JDK
+    }
+}
+
+javafx {
+    version = "17.0.10"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+application {
+    // If you have module-info.java, also set mainModule below.
+    mainClass.set("org.example.Output")
+    mainModule.set("org.example")
+    // mainModule.set("edu.bsu.cs") // <- uncomment if you have a module-info.java named 'module edu.bsu.cs { ... }'
 }
